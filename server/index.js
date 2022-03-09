2cc4e24ed19452487dba2a1f910a7f154c41a871
siddharthgopakumar <61907100+siddharthgopakumar@users.noreply.github.com>
2022-03-09 21:22:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://RussianPriestRasputin:preachbiblelikeapreacher@cluster0.70hnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://xxxxxxxxxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxx@cluster0.70hnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
